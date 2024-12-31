767d9315b8d95bb0bc4a60f389d9d9613e8072dd
Johnny Estrada <johnnye562@yahoo.com>
2024-12-31 13:37:13 -0800

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+
+const userNavigation = [
+  { name: "Your profile", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 sm:gap-x-6 sm:px-6 lg:px-10 bg-stone-100">
+      <div className="flex justify-end items-center flex-1 gap-x-4 self-stretch lg:gap-x-6">
+        <div className="flex items-center gap-x-4 lg:gap-x-6">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+          >
+            <span className="sr-only">View notifications</span>
+            <BellIcon aria-hidden="true" className="size-6" />
+          </button>
+
+          {/* Profile dropdown */}
+          <Menu as="div" className="relative">
+            <MenuButton className="-m-1.5 flex items-center p-1.5">
+              <span className="sr-only">Open user menu</span>
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="size-8 rounded-full bg-gray-50"
+              />
+            </MenuButton>
+            <MenuItems
+              transition
+              className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              {userNavigation.map((item) => (
+                <MenuItem key={item.name}>
+                  <a
+                    href={item.href}
+                    className="block px-3 py-1 text-sm/6 text-gray-900 data-[focus]:bg-gray-50 data-[focus]:outline-none"
+                  >
+                    {item.name}
+                  </a>
+                </MenuItem>
+              ))}
+            </MenuItems>
+          </Menu>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
