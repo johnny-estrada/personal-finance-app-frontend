1d96e25815c481a4782533949be0b2cfcbc9da3d
Johnny Estrada <johnnye562@yahoo.com>
2025-01-01 12:54:30 -0800

    Create RecurringBills page

---
+++ src/pages/RecurringBills.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import Heading from "../components/ui/Heading";
+
+export default function RecurringBills() {
+  return (
+    <div>
+      <Sidebar />
+      <div className="lg:pl-[300px]">
+        <Header />
+
+        <main>
+          {/* Main content */}
+          <Heading className="px-10">Recurring Bills</Heading>
+          <div className="px-4 sm:px-6 lg:px-10 lg:pt-8">
+            <div className="flex py-8 gap-6">content goes here</div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
