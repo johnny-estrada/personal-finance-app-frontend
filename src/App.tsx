4d4f47f399e42562266dc7305be4cd06d78c6395
Johnny Estrada <johnnye562@yahoo.com>
2025-01-01 12:52:45 -0800

    Add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Overview from "./pages/Overview";
 import Transactions from "./pages/Transactions";
+import Budgets from "./pages/Budgets";
+import Pots from "./pages/Pots";
+import RecurringBills from "./pages/RecurringBills";
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
       <Routes>
         <Route index={true} path="/" element={<Overview />} />
         <Route path="/transactions" element={<Transactions />} />
+        <Route path="/budgets" element={<Budgets />} />
+        <Route path="/pots" element={<Pots />} />
+        <Route path="/recurring-bills" element={<RecurringBills />} />
       </Routes>
     </BrowserRouter>
   );
