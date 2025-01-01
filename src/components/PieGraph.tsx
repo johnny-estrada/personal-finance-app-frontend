036e8ac3cc670efa896d06050f5aa40d93495bd2
Johnny Estrada <johnnye562@yahoo.com>
2025-01-01 11:59:24 -0800

    Format pie graph component

--- src/components/PieGraph.tsx
+++ src/components/PieGraph.tsx
@@ -1,22 +1,23 @@
-import React, { PureComponent } from 'react';
-import { ResponsiveContainer, PieChart, Pie } from 'recharts';
+import React, { PureComponent } from "react";
+import { ResponsiveContainer, PieChart, Pie } from "recharts";
 
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
 ];
 
 export default class PieGraph extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/p/sandbox/pie-chart-in-responsive-container-dmhf62';
+  static demoUrl =
+    "https://codesandbox.io/p/sandbox/pie-chart-in-responsive-container-dmhf62";
 
   render() {
     return (
-      <div style={{ width: '100%', height: 300 }}>
+      <div style={{ width: "100%", height: 300 }}>
         <ResponsiveContainer>
           <PieChart>
-            <Pie dataKey="value" data={data} fill="#82C9D7"/>
+            <Pie dataKey="value" data={data} fill="#82C9D7" />
           </PieChart>
         </ResponsiveContainer>
       </div>
