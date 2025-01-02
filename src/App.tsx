400d8c8f0d0ea4b3a80d1007129807b19e340960
Johnny Estrada <johnnye562@yahoo.com>
2025-01-01 16:19:23 -0800

    Update imports to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Overview from "./pages/Overview";
-import Transactions from "./pages/Transactions";
-import Budgets from "./pages/Budgets";
-import Pots from "./pages/Pots";
-import RecurringBills from "./pages/RecurringBills";
+import Overview from "./pages/Overview/Overview";
+import Transactions from "./pages/Transactions/Transactions";
+import Budgets from "./pages/Budgets/Budgets";
+import Pots from "./pages/Pots/Pots";
+import RecurringBills from "./pages/RecurringBills/RecurringBills";
 
 export default function App() {
   return (
