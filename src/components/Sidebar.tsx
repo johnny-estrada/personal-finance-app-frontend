b5c1f4bb8c8539670f0936db67fd040e33766fe6
Johnny Estrada <johnnye562@yahoo.com>
2025-01-04 11:19:18 -0800

    Add sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,189 +1,19 @@
-import { useState } from "react";
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  TransitionChild,
-} from "@headlessui/react";
-import { Cog6ToothIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router";
-
-const navigation = [
-  {
-    name: "Overview",
-    href: "/",
-    icon: "src/assets/icons/icon-nav-overview.svg",
-    current: true,
-  },
-  {
-    name: "Transactions",
-    href: "/transactions",
-    icon: "src/assets/icons/icon-nav-transactions.svg",
-    current: false,
-  },
-  {
-    name: "Budgets",
-    href: "/budgets",
-    icon: "src/assets/icons/icon-nav-budgets.svg",
-    current: false,
-  },
-  {
-    name: "Pots",
-    href: "/pots",
-    icon: "src/assets/icons/icon-nav-pots.svg",
-    current: false,
-  },
-  {
-    name: "Recurring bills",
-    href: "/recurring-bills",
-    icon: "src/assets/icons/icon-nav-recurring-bills.svg",
-    current: false,
-  },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import Navigation from "./ui/Navigation";
+import MobileNavigation from "./ui/MobileNavigation";
+import Logo from "./ui/Logo";
 
 const Sidebar = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
-      <Dialog
-        open={sidebarOpen}
-        onClose={setSidebarOpen}
-        className="relative z-50 lg:hidden"
-      >
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
-        />
-
-        <div className="fixed inset-0 flex">
-          <DialogPanel
-            transition
-            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
-          >
-            <TransitionChild>
-              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
-                <button
-                  type="button"
-                  onClick={() => setSidebarOpen(false)}
-                  className="-m-2.5 p-2.5"
-                >
-                  <span className="sr-only">Close sidebar</span>
-                  <XMarkIcon aria-hidden="true" className="size-6 text-white" />
-                </button>
-              </div>
-            </TransitionChild>
-            {/* Sidebar component, swap this element with another sidebar if you like */}
-            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
-              <div className="flex h-16 shrink-0 items-center">
-                <img
-                  alt="Your Company"
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-auto"
-                />
-              </div>
-              <nav className="flex flex-1 flex-col">
-                <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                  <li>
-                    <ul role="list" className="-mx-2 space-y-1">
-                      {navigation.map((item) => (
-                        <li key={item.name}>
-                          <Link
-                            to={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-800 text-white"
-                                : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                              "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold",
-                            )}
-                          >
-                            <img
-                              src={item.icon}
-                              aria-hidden="true"
-                              className="size-6 shrink-0"
-                            />
-                            {item.name}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-
-                  <li className="mt-auto">
-                    <Link
-                      to="#"
-                      className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-400 hover:bg-gray-800 hover:text-white"
-                    >
-                      <Cog6ToothIcon
-                        aria-hidden="true"
-                        className="size-6 shrink-0"
-                      />
-                      Settings
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </DialogPanel>
-        </div>
-      </Dialog>
-
+      <MobileNavigation />
       {/* Static sidebar for desktop */}
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-[300px] lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-neutral-800 px-6 pb-4 rounded-r-2xl">
           <div className="flex lg:px-3 lg:py-10 shrink-0">
-            <img
-              alt="finance"
-              src="src/assets/icons/logo-large.svg"
-              className="h-5 w-auto"
-            />
+            <Logo />
           </div>
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-              <li>
-                <ul role="list" className="-ml-6 -mr-.5 space-y-1">
-                  {navigation.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        to={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-stone-100 text-neutral-800 border-l-4 border-teal-700"
-                            : "text-zinc-400 hover:text-white",
-                          "group flex gap-x-3 rounded-r-xl px-8 py-4 font-semibold",
-                        )}
-                      >
-                        <img
-                          src={item.icon}
-                          aria-hidden="true"
-                          className="size-6 shrink-0"
-                        />
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-
-              <li className="mt-auto">
-                <Link
-                  to="#"
-                  className="group -mx-2 flex gap-x-3 rounded-md p-2 font-semibold text-zinc-400 hover:text-white"
-                >
-                  <img
-                    src="src/assets/icons/icon-minimize-menu.svg"
-                    aria-hidden="true"
-                    className="size-6 shrink-0"
-                  />
-                  Minimize Menu
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <Navigation />
         </div>
       </div>
     </>
