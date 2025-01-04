cef34fa08a60ef4faf0e213ea871ecf90df24e01
Johnny Estrada <johnnye562@yahoo.com>
2025-01-04 13:40:27 -0800

    Add custom colors from style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
     './src/**/*.{ts,tsx}',],
   theme: {
     extend: {},
+    colors: {
+      'beige-500': '#98908B',
+      'beige-100': '#F8F4F0',
+      'grey-900': '#201F24',
+      'grey-500': '#696868',
+      'grey-300': '#B3B3B3',
+      'grey-100': '#F2F2F2',
+      'white': '#FFF',
+      'green': '#277C78',
+      'yellow': '#F2CDAC',
+      'cyan': '#82C9D7',
+      'navy': '#626070',
+      'red': '#C94736',
+      'purple': '#826CB0',
+    }
   },
   plugins: [],
 }
