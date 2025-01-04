2f82ac1d214a9ad7d3f1ac76027b628ac8b6e660
Johnny Estrada <johnnye562@yahoo.com>
2025-01-04 11:20:30 -0800

    Update variant styles

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -2,9 +2,8 @@ import React, { HTMLAttributes, ElementType, ReactNode } from "react";
 import classNames from "classnames";
 
 const variantStyles = {
-  h1: "text-4xl font-bold",
-  h2: "text-3xl font-semibold",
-  h3: "text-2xl font-medium",
+  h1: "text-[32px] font-bold",
+  h2: "text-xl font-bold", //20px
 };
 
 type Variant = keyof typeof variantStyles;
