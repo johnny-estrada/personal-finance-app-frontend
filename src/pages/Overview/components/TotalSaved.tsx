53139c5d6c0239196d84872dc651569d7cbcfaab
Johnny Estrada <johnnye562@yahoo.com>
2025-01-04 11:23:34 -0800

    Create TotalSaved component

---
+++ src/pages/Overview/components/TotalSaved.tsx
@@ -0,0 +1,13 @@
+const TotalSaved = () => {
+  return (
+    <>
+      <img className="w-auto h-10" src="src/assets/icons/icon-pot.svg" />
+      <div className="ml-4">
+        <p className="text-sm text-stone-500 mb-3">Total Saved</p>
+        <p className="text-[32px] font-bold">$850</p>
+      </div>
+    </>
+  );
+};
+
+export default TotalSaved;
